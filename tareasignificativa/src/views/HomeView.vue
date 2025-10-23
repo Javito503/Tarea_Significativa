9ae43b919af94372c39ffa9f47f0485a1ad9ef95
Javito503 <javieralex.jar@gmail.com>
2025-10-23 09:22:33 -0600

    import cursodiplomado e investigacion social al homeview

--- tareasignificativa/src/views/HomeView.vue
+++ tareasignificativa/src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CursosDiplomados/>
+    <InvestigacionSocial/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import CursosDiplomados from '@/components/CursosDiplomados.vue'
+import InvestigacionSocial from '@/components/InvestigacionSocial.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    CursosDiplomados,
+    InvestigacionSocial
   }
 }
 </script>
