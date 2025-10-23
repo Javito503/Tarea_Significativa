c560ab37bd804470444b2877962e610ae6b1975a
Javito503 <javieralex.jar@gmail.com>
2025-10-23 10:11:00 -0600

    oe

--- tareasignificativa/src/views/HomeView.vue
+++ tareasignificativa/src/views/HomeView.vue
@@ -1,23 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <CursosDiplomados/>
-    <InvestigacionSocial/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import CursosDiplomados from '@/components/CursosDiplomados.vue'
-import InvestigacionSocial from '@/components/InvestigacionSocial.vue';
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
-    CursosDiplomados,
-    InvestigacionSocial
   }
 }
 </script>
